f4a12289d768a95f8d7bde603ab3c394015781f9
khalid hassan <abuuanas30@hotmail.com>
2017-12-10 23:17:16 +0300

    ading ride sharing functionality

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carpool.settings")            
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carpool.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
@@ -14,7 +14,7 @@ if __name__ == "__main__":
             import django
         except ImportError:
             raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
+                "Couldn't import Django. Are you sure it's installed and "         
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
             )
